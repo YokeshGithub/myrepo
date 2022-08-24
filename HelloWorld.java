2d876adbc6a9d6a5d136ada7490767e5730b2c28
YokeshGithub <111746788+YokeshGithub@users.noreply.github.com>
2022-08-24 11:58:26 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+// This is a simple Java program.
+// FileName : "HelloWorld.java".
+package mypkg;
+
+public class HelloWorld {
+	// Your program begins with a call to main().
+    // Prints "Hello, World" to the terminal window.
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+
+	}
+
+}
