a4b68aace7250771ae411d4403fbff8cf3ef7887
YokeshGithub <111746788+YokeshGithub@users.noreply.github.com>
2022-08-24 13:44:52 +0530

    Add files via upload

---
+++ MyNameChar.java
@@ -0,0 +1,27 @@
+package mypkg;
+
+public class MyNameChar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char a = '\u0059';
+		char b = '\u004f';
+		char c = '\u004b';
+		char d = '\u0045';
+		char e = '\u0053';
+		char f = '\u0048';
+		char g = '\u0056';
+		System.out.println("Captial Letter of my name using CHAR value:" +a+b+c+d+e+f+" "+g);
+		char h = '\u0079';
+		char i = '\u006f';
+		char j = '\u006b';
+		char k = '\u0065';
+		char l = '\u0073';
+		char m = '\u0068';
+		char n = '\u0076';
+		char v = ' ';
+		System.out.println("Small  Letter of my name using CHAR value:" +h+i+j+k+l+m+v+n);
+
+	}
+
+}
